42edeac4392a6c72d5dae6f6b65072966bc71a8c
Antoneil Singh <antoneilsingh@gmail.com>
2024-02-23 13:03:48 -0500

    Added CSS and datetime function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import datetime
 
 ###
 # Routing for your application.
@@ -31,15 +31,25 @@ def send_text_file(file_name):
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
-    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
 
+
+@app.route('/profile')
+def profile():
+    date_joined = datetime.now() 
+    formatted_date_joined = format_date_joined(date_joined)
+
+    return render_template('profile.html', date_joined=formatted_date_joined)
+
+
+def format_date_joined(date_joined):
+    return date_joined.strftime('%b, %Y')
+
+
+def format_date_joined(date_joined):
+    return date_joined.strftime('%b, %Y')
 
 @app.errorhandler(404)
 def page_not_found(error):
